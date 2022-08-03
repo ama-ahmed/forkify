018ce924c1c937930dbd2fb3f3a98bc8c38069fc
ama-ahmed <ama.ahmed.mohammed@gmail.com>
2022-08-03 03:44:13 +0300

    deploy

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '631094d4-b65b-46e0-bed5-5af25f8c87c4';
+export const MODAL_CLOSE_SEC = 2.5;
+
